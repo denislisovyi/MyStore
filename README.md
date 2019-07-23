b492d23c3221ee513b04a14f82f87cee741ce1e2
denislisovyi <32895041+denislisovyi@users.noreply.github.com>
2019-07-23 15:44:14 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# MyStore
-MyStore
+translate
+=========
+OpenCart Translate
+
+License:
+GNU General Public License version 3 (GPLv3)
+
+OpenCart Ukrainian Repository:
+http://github.com/opencart-ukraine
+
+OpenCart Ukrainian Community:
+http://opencart-ukraine.tumblr.com
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/store/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/store/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/store/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/store/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/store/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/store/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/store/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/store/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/store/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/store/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/store/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/store/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/store/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'denisfcdk1997');
+define('DB_DATABASE', 'store');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
